2eb70b45c46ab23fd0badb12689da64c7c3e46a5
uitox_macbook <JerryLiao@uitox.com>
2015-09-27 17:21:15 +0800

    Initial Commit

---
+++ CookieForWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookieForWebView
+//
+//  Created by uitox_macbook on 2015/9/27.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
